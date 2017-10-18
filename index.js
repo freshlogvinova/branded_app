64fadf3a64653f3c0382f1dc958290fdd8097a24
logvinova <logvinova.freshcode@gmail.com>
2017-10-18 16:40:17 +0300

    index.js: Added the logger ( morgan) and the try/catch

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const fs = require('fs');
-const upload = require('./controller/uploads')
+const upload = require('./controller/uploads');
 const favicon = require('serve-favicon');
 const rimraf = require('rimraf');
+const logger = require('morgan');
 
 //Connection to mysql
 const connection = require('./controller/db_connect');
@@ -24,60 +24,81 @@ app.use(bodyParser.json())
     })
     .use(express.static(__dirname + '/public'))
     .use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')))
+    .use(logger('dev'))
     .use('/uploads', express.static(__dirname + "/uploads"))
     .get('/get_all_items', function (req, res) {
-      connection.query('SELECT * FROM items', function (err, rows, fields) {
-        if (err) {
-          console.log("ERROR", err);
-        }
-        else {
-          res.send(rows);
-        }
-      });
-    })
-    .get('/get_pages', function (req, res) {
-      connection.query('SELECT * FROM page', function (err, rows, fields) {
-        if (err) {
-          console.log("ERROR", err);
-        }
-        else
-          res.send(rows);
-      });
-    })
-    .post('/upload', upload, function (req, res) {
-      if (!!req.file.mimetype && (!!~mimetypeSuccess.indexOf(req.file.mimetype)) && !!req.body.select) {
-        connection.query('INSERT INTO items(link, page) VALUES (?, ?)', [req.file.path, req.body.select], function (err, rows, fields) {
+      try {
+        connection.query('SELECT * FROM items', function (err, rows, fields) {
           if (err) {
             console.log("ERROR", err);
-            res.send("file didn't save on server");
           }
           else {
-            res.send("file saved on server");
+            res.send(rows);
           }
         });
       }
-      else {
-        res.send("file didn't save on server");
+      catch (err) {
+        console.log(err)
       }
     })
-    .delete('/remove_video/:id', function (req, res) {
-      connection.query('SELECT * FROM items WHERE id= ?', [req.params.id], function (err, rows, fields) {
-        const path = rows[0].link;
-        if (err) {
-          console.log("ERROR", err)
-        }
-        else {
-          rimraf('./' + path, function () {
-            console.log("file was deleted");
+    .get('/get_pages', function (req, res) {
+      try {
+        connection.query('SELECT * FROM page', function (err, rows, fields) {
+          if (err) {
+            console.log("ERROR", err);
+          }
+          else
+            res.send(rows);
+        });
+      } catch (err) {
+        console.log(err)
+      }
+
+    })
+    .post('/upload', upload, function (req, res) {
+      try {
+        if (!!req.file.mimetype && (!!~mimetypeSuccess.indexOf(req.file.mimetype)) && !!req.body.select) {
+          connection.query('INSERT INTO items(link, page) VALUES (?, ?)', [req.file.path, req.body.select], function (err, rows, fields) {
+            if (err) {
+              console.log("ERROR", err);
+              res.send("file didn't save on server");
+            }
+            else {
+              res.send("file saved on server");
+            }
           });
         }
-      });
-
-      connection.query('DELETE FROM items WHERE id= ?', [req.params.id], function (err, result) {
-        if (err) {
-          console.log("ERROR", err)
+        else {
+          res.send("file didn't save on server");
         }
-      });
+      } catch (err) {
+        console.log(err)
+      }
+
+    })
+    .delete('/remove_video/:id', function (req, res) {
+      try {
+        connection.query('SELECT * FROM items WHERE id= ?', [req.params.id], function (err, rows, fields) {
+          const path = rows[0].link;
+          if (err) {
+            console.log("ERROR", err)
+          }
+          else {
+            rimraf('./' + path, function () {
+              console.log("file was deleted");
+            });
+          }
+        });
+
+        connection.query('DELETE FROM items WHERE id= ?', [req.params.id], function (err, result) {
+          if (err) {
+            console.log("ERROR", err)
+          }
+        });
+      }
+      catch (err) {
+        console.log(err)
+      }
     });
 
 const listener = app.listen(3001, function () {
